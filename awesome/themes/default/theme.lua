513aac9380e8cce3d04f3efba553aef6281fee79
Igor Cieply <igoracieply@outlook.com>
2023-11-15 14:21:35 -0600

    Adjustments

--- awesome/themes/default/theme.lua
+++ awesome/themes/default/theme.lua
@@ -7,7 +7,7 @@ local xresources = require("beautiful.xresources")
 local dpi = xresources.apply_dpi
 
 local gfs = require("gears.filesystem")
-local themes_path = gfs.get_themes_dir()
+local themes_path = "~/.config/awesome/themes/"
 
 local theme = {}
 
@@ -99,7 +99,7 @@ theme.titlebar_maximized_button_focus_inactive  = themes_path.."default/titlebar
 theme.titlebar_maximized_button_normal_active = themes_path.."default/titlebar/maximized_normal_active.png"
 theme.titlebar_maximized_button_focus_active  = themes_path.."default/titlebar/maximized_focus_active.png"
 
-theme.wallpaper = "~/Pictures/pexels-sohail-nachiti-807598.jpg"
+theme.wallpaper = themes_path.."default/mainback.png"
 
 -- You can use your own layout icons like this:
 theme.layout_fairh = themes_path.."default/layouts/fairhw.png"
